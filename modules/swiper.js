b9056bed0ce7b1856f95c16b04b4ef8646b4d0b6
zacker101 <tombson.m@gmail.com>
2024-08-11 11:40:09 +0300

    added nav

--- modules/swiper.js
+++ modules/swiper.js
@@ -1,5 +1,9 @@
 export  function swiperInit(){
   var swiper = new Swiper(".swiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
     // pagination: {
     //   el: ".swiper-pagination",
     //   clickable:true
